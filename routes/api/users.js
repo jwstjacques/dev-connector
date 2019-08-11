72c24ee08fbeb3fe48783d781265d82ae1717585
Jason St. Jacques <jw.stjacques@gmail.com>
2019-08-11 15:53:30 -0400

    Validation on register and login

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,6 +11,7 @@ const User = require('../../models/User');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // @route GET api/users
 // @desc
@@ -73,6 +74,13 @@ router.post('/register', (req, res) => {
 // @desc Login User / Returning JWT token
 // @access Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation of body
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -80,7 +88,8 @@ router.post('/login', (req, res) => {
   User.findOne({ email }).then((user) => {
     //Check for user
     if (!user) {
-      res.status(404).json({ email: 'User not found' });
+      errors.email = 'User not found';
+      return res.status(400).json(errors);
     }
     // Check password
     bcrypt.compare(password, user.password).then((isMatch) => {
@@ -103,7 +112,8 @@ router.post('/login', (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: 'Password incorrect' });
+        errors.password = 'Password incorrect';
+        return res.status(400).json(errors);
       }
     });
   });
