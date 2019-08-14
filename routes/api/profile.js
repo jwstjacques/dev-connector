46869c95655204292ce0b5cd9b9e1cd53abe0293
Jason St. Jacques <jw.stjacques@gmail.com>
2019-08-14 00:11:14 -0400

    Created delete education and experience routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -269,4 +269,68 @@ router.post(
   }
 );
 
+// @route POST api/profile/experience/:exp_id
+// @desc POST Delete experience from profile
+// @access Private
+router.delete(
+  '/experience/:exp_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        if (!profile) {
+          errors.profile = 'Profile does not exist';
+          return res.status(404).json(errors);
+        }
+
+        const removeIndex = profile.experience
+          .map((item) => item.id)
+          .indexOf(req.params.exp_id);
+
+        // Splice out of array
+        profile.experience.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then((profile) => {
+          res.json(profile);
+        });
+      })
+      .catch((err) => res.status(404).json(err));
+  }
+);
+
+// @route POST api/profile/education/:edu_id
+// @desc POST Delete education from profile
+// @access Private
+router.delete(
+  '/education/:edu_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        if (!profile) {
+          errors.profile = 'Profile does not exist';
+          return res.status(404).json(errors);
+        }
+
+        const removeIndex = profile.education
+          .map((item) => item.id)
+          .indexOf(req.params.edu_id);
+
+        // Splice out of array
+        profile.education.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then((profile) => {
+          res.json(profile);
+        });
+      })
+      .catch((err) => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
