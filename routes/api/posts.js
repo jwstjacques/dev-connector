888be04fa514408f90db374371654b9489e88836
Jason St. Jacques <jw.stjacques@gmail.com>
2019-08-14 18:28:17 -0400

    GET all and DELETE routes for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,15 +4,70 @@ const passport = require('passport');
 const router = express.Router();
 
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 const validatePostInput = require('../../validation/post');
 
 // @route GET api/posts
-// @desc
+// @desc GET Posts
 // @access Public
 router.get('/', (req, res) => {
-  res.json({ msg: 'Posts Works' });
+  Post.find()
+    .sort({ date: -1 })
+    .then((posts) => res.json(posts))
+    .catch((err) => res.status(404).json({ nopostsfound: 'No posts found' }));
 });
+
+// @route GET api/posts/:id
+// @desc GET post by id
+// @access Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => res.json(post))
+    .catch((err) =>
+      res.status(404).json({ nopostfound: 'No post found with that id' })
+    );
+});
+
+// @route DELETE api/posts/:id
+// @desc DELETE post by id
+// @access Public
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        if (!profile) {
+          errors.userdoesnotexist = 'User does not exist';
+          return res.status(404).json({ errors });
+        }
+
+        Post.findById(req.params.id).then((post) => {
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: 'User not authorized' });
+          }
+
+          // Delete
+          post
+            .remove()
+            .then(() => {
+              res.json({ success: true });
+            })
+            .catch((err) => {
+              res.status(404).json({ postnotfound: 'No post found' });
+            });
+        });
+      })
+      .catch((err) =>
+        res.status(404).json({ nopostfound: 'No post found with that id' })
+      );
+  }
+);
 
 // @route POST api/posts
 // @desc POST Create post
