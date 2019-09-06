15505f231f5eae46e34e32dccfe5234ff84addc6
Jason St. Jacques <jw.stjacques@gmail.com>
2019-09-06 16:12:51 -0400

    Basic react nav, main page and footer, with no functionality

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Landing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
